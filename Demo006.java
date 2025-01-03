7d5215e2d19c91f347df9553e9b47bf8b7c0d3bc
adhithya-2005 <adithyakemidikemidiadithya@gmail.com>
2025-01-03 10:59:48 +0530

    while-username

---
+++ Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006 {
+    public static void main(String[] args){
+        String name = "adhithya";
+        String pwd = "adhithya@123";
+        while ((name == "adhithya") && (pwd == "adhithya 123")){
+            System.out.println("Hello adhithya welcome to malla reddy");
+            break;
+        }
+    }
+}
