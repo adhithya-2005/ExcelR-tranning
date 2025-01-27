41c64c8ac6396ca7d70a2f9181cefe556bc1c60d
adhithya-2005 <adithyakemidikemidiadithya@gmail.com>
2025-01-27 22:29:42 +0530

    To print 5 table

---
+++ Demo050.java
@@ -0,0 +1,9 @@
+public class Demo050 {
+    public static void main(String[] args) {
+        
+        System.out.println("Table of 5:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("5 x " + i + " = " + (5 * i));
+        }
+    }
+}
